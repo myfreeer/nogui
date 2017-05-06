086f394aec7729deb29c7322eff9d6c79936f721
myfreeer <myfreeer@users.noreply.github.com>
2017-05-06 17:50:57 +0800

    autocrop: enable hwaccel

--- autocrop.sh
+++ autocrop.sh
@@ -1,2 +1,2 @@
 #!sh
-./ffmpeg -ss 600 -i $1 -t 120 -sn -dn -an -vf cropdetect=24:2 -f null nul 2>&1 | awk '/crop/ { print $NF }' | tail -1
+./ffmpeg -ss 600 -hwaccel auto -i $1 -t 120 -sn -dn -an -vf cropdetect=24:2 -f null nul 2>&1 | awk '/crop/ { print $NF }' | tail -1
